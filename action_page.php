6494b644b754fe78cb8e130a68f00c5a213ecf9d
Tarun <tarunsaiks@gmail.com>
2020-05-27 23:16:05 +0530

    Tested Final

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<?php
+ echo "THANKS BHAI"
+
+?>
+<html>
+	<head>Your Response has been recieved</head>
+		<body>
+			<h1>THANK YOU</h1>
+		</body>
+</html>
---
+++ templates/register/action_page.php
@@ -0,0 +1,6 @@
+<html>
+	<head>Your Response has been recieved/head>
+		<body>
+			<h1>THANK YOU</h1>
+		</body>
+</html>
